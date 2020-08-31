effbe1080526b92f24eccf1dca53d500d73fe497
Eugene Stepanenko <stepanenko.yevheniy@gmail.com>
2020-08-31 18:48:48 +0300

    Included production build

---
+++ build/precache-manifest.b95216803b719402c871a33f9fa72c1f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d83579a8c0da070a2dff598668ca8bd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "beb5f7a6e64a3514ff1f",
+    "url": "/static/css/2.855e8389.chunk.css"
+  },
+  {
+    "revision": "effbcab60d79652edfcf",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "beb5f7a6e64a3514ff1f",
+    "url": "/static/js/2.154496f6.chunk.js"
+  },
+  {
+    "revision": "c560b744118e776c5a9fe14e0546e12a",
+    "url": "/static/js/2.154496f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "effbcab60d79652edfcf",
+    "url": "/static/js/main.666b2b04.chunk.js"
+  },
+  {
+    "revision": "09c4bf2ba5891ec32806",
+    "url": "/static/js/runtime-main.f04e1819.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
